922384f99be1e912d3e2fc02309f11034515e720
Bartek Kus <7887446+bartekus@users.noreply.github.com>
2025-06-11 01:55:54 -0600

    Baseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
 import App from './App.tsx'
+import { Home } from "./pages/home.tsx";
+import { Contact } from "./pages/contact.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/bartekus.com",
+        element: <App />,
+        children: [
+            {
+                path: "/bartekus.com",
+                element: <Home />,
+            },
+            {
+                path: "/bartekus.com/contact",
+                element: <Contact />,
+            },
+        ],
+    },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )

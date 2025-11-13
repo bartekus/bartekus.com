05350ada79e2d8d802067b8471499a42e180d457
Bartek Kus <7887446+bartekus@users.noreply.github.com>
2025-11-13 15:45:46 -0700

    Add dependencies | Adjust vite config | Add misc files

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,22 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import mdx from "@mdx-js/rollup";
+import remarkGfm from "remark-gfm";
+import rehypePrism from "rehype-prism-plus";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    host: "::",
+    port: 5173,
+  },
+  plugins: [
+    mdx({
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [rehypePrism],
+    }),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
 });

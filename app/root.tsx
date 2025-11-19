4ffe7d07f7b5b653406216e68083d8c516164fe2
Bartek Kus <7887446+bartekus@users.noreply.github.com>
2025-11-19 13:10:52 -0700

    Optimization

--- app/root.tsx
+++ app/root.tsx
@@ -12,20 +12,20 @@ import { Umami } from "~/components/analytics/Umami";
 
 import "./app.css";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "dns-prefetch", href: "https://fonts.googleapis.com" },
-  { rel: "dns-prefetch", href: "https://fonts.gstatic.com" },
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+// export const links: Route.LinksFunction = () => [
+//   { rel: "dns-prefetch", href: "https://fonts.googleapis.com" },
+//   { rel: "dns-prefetch", href: "https://fonts.gstatic.com" },
+//   { rel: "preconnect", href: "https://fonts.googleapis.com" },
+//   {
+//     rel: "preconnect",
+//     href: "https://fonts.gstatic.com",
+//     crossOrigin: "anonymous",
+//   },
+//   {
+//     rel: "stylesheet",
+//     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+//   },
+// ];
 
 export function meta(_args: Route.MetaArgs) {
   return [

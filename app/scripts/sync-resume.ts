3f42fc422914afe84d3379fc24fac8b72e3c2db7
Bartek Kus <7887446+bartekus@users.noreply.github.com>
2026-03-25 15:13:28 -0600

    feat(upkeep): update work

--- app/scripts/sync-resume.ts
+++ app/scripts/sync-resume.ts
@@ -1,7 +1,7 @@
 import fs from "node:fs/promises";
 
 const RESUME_URL =
-  "https://gist.githubusercontent.com/bartekus/d2f989f89d590ad0e661245ca18cd0df/raw/1b08800a1de921dc8f6e7c073be5fe9c9836ef94/resume.json";
+  "https://gist.githubusercontent.com/bartekus/d2f989f89d590ad0e661245ca18cd0df/raw/90858b148b5d88ddddae8867866f009961d384ae/resume.json";
 
 async function main() {
   console.log("Syncing resume.json from gist...");
